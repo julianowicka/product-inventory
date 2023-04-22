82f76b368b8814e8121009290694f09942741546
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-22 11:34:41 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductListComponent} from "./product-list/product-list.component";
+import {AddProductComponent} from "./add-product/add-product.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "products",
+    pathMatch: "full"
+  },
+  {
+    path: "products",
+    component: ProductListComponent,
+
+  },
+
+  {
+    path: "add",
+    component: AddProductComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "products",
+
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
