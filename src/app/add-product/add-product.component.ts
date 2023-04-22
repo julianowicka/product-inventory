39abfa4bdb8a24b6dff19c68d958985c6a5d9ebd
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-22 14:40:52 +0200

    Add possibility of adding product

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {Product} from "../product-list/product-model";
+import {ProductService} from "../product-list/product-service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add-product',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent {
+  constructor(private productService: ProductService,
+    private router: Router){
+  }
+  addProduct(productForm: NgForm){
+    const product = new Product();
+    product.name = productForm.value.name;
+    product.description = productForm.value.description;
+    product.price = productForm.value.price;
+    product.quantity = productForm.value.quantity;
 
+    this.productService.addProduct(product)
+    console.log("product");
+    console.log(product);
+  }
+
+  protected readonly onsubmit = onsubmit;
 }
