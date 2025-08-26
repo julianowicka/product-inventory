e41391db6b9c559c02e23281e86946c6e12ad165
Julia Nowicka <julia.nowicka@lemondev.pl>
2025-08-26 12:21:51 +0200

    refactor(components): migrate to modern Angular inject() pattern

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,29 +1,26 @@
-import { Component } from '@angular/core';
-import {NgForm} from "@angular/forms";
-import {Product} from "../product-list/product-model";
-import {ProductService} from "../product-list/product-service";
-import {Router} from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Product } from '../product-list/product-model';
+import { ProductService } from '../product-list/product-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.scss']
+  styleUrls: ['./add-product.component.scss'],
 })
 export class AddProductComponent {
-  constructor(private productService: ProductService,
-    private router: Router){
-  }
-  addProduct(productForm: NgForm){
+  private productService = inject(ProductService);
+  private router = inject(Router);
+  addProduct(productForm: NgForm) {
     const product = new Product();
     product.name = productForm.value.name;
     product.description = productForm.value.description;
     product.price = productForm.value.price;
     product.quantity = productForm.value.quantity;
 
-    this.productService.addProduct(product)
-    console.log("product");
+    this.productService.addProduct(product);
+    console.log('product');
     console.log(product);
   }
-
-  protected readonly onsubmit = onsubmit;
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,29 +1,29 @@
-import {Component, OnInit} from '@angular/core';
-import {Product} from "./product-model";
-import {ProductService} from "./product-service";
+import { Component, OnInit, inject } from '@angular/core';
+import { Product } from './product-model';
+import { ProductService } from './product-service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
   products: Product[] = [];
-  constructor(private productService: ProductService) {
-  }
+  private productService = inject(ProductService);
   ngOnInit(): void {
     this.products = this.productService.getProducts();
 
-    for (let i = 0; i<10; i++){
-      const product = new Product()
+    for (let i = 0; i < 10; i++) {
+      const product = new Product();
       product.name = `Product ${i}`;
-      product.description = `Descrition ${i}`;
+      product.description = `Description ${i}`;
       product.price = i + 1;
-      product.quantity = i +1;
-      this.products.push(product)
+      product.quantity = i + 1;
+      this.products.push(product);
     }
   }
+
   deleteProducts(id?: number): void {
-  this.productService.deleteProducts(id)
-}
+    this.productService.deleteProducts(id);
+  }
 }
